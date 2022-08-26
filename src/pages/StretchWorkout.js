a19e773df85127eb62ceebff52a87daabb085cdd
Lilly <lilly@DESKTOP-I6QR8AT.(none)>
2022-08-26 16:24:38 +0200

    styling and pause btn

--- src/pages/StretchWorkout.js
+++ src/pages/StretchWorkout.js
@@ -10,15 +10,13 @@ import stopBtnPic from "../pictures/pause.png";
 import ProgressBar from "../components/progress-bar.component";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 const StretchWorkout = (props) => {
   const [btnStatusStart, setBtnStatusStart] = React.useState(false);
   const [btnStatusStop, setBtnStatusStop] = React.useState(true);
   const [exercise, setExercise] = useState(allExercises);
   const [number, setNumber] = useState(0);
-
 
   const [timer, setTimer] = React.useState(exercise[number].duration);
   const audio = new Audio(timesUpSound);
@@ -27,132 +25,89 @@ const StretchWorkout = (props) => {
   const { isAuthenticated } = useAuth0();
   const [key, setKey] = useState(0);
 
-  //Use RefHook to make it stop at 0
-  //stores reference to SetInterval --> clear when timer hits 0
-  const id = React.useRef(null);
-  const clear = () => {
-    window.clearInterval(id.current);
-   
-  };
+  const [srcPic, setSrcPic] = useState("Pause")
 
   //Start Timer on Button Click --> startBtn
   const startTimer = () => {
-
-    setStartedTimer(true)
-
-   /*  id.current = setInterval(() => {
-      setTimer((time) => time - 1);
-    
-    }, 1000);
-    
-    setBtnStatusStart(true);
-    setBtnStatusStop(false);
-    return () => clear(); */
+    setStartedTimer(true);
+    setKey((prevKey) => prevKey + 1);
+    setTimer(exercise[number].duration)
+    setBtnStatusStart(true)
   };
 
-  //Stop Timer when time is up, set new Time so countdown starts again
-  React.useEffect(() => {
-
-    if (timer === 0 && number <= 21) {
-      clear();
-      setTimer(exercise[number].duration);
-      setNumber(number + 1);
-      startTimer();
-
-    } else if (timer === 0 && number > 22) {
-      clear();
-      setTimer(0);
-      setNumber(number + 1);
-     
-    }
-  }, [timer]);
-
-  //Stop Timer on Button click -->  stopBtn
+  //Stop/Play on Button click
   const stopTimer = () => {
-
-    setStartedTimer(false)
-/*     clear();
-    console.log("test music btn")
-    setBtnStatusStart(false);
-    setBtnStatusStop(true); */
+    startedTimer ? stop() : play();
   };
+
+
+          const stop =() => {
+            setStartedTimer(false)
+            setSrcPic("Go on")
+          }
+
+          const play =() => {
+            setStartedTimer(true)
+            setSrcPic("Pause")
+          }
+
 
   const renderTime = ({ remainingTime }) => {
     if (remainingTime === 0) {
-      return <div className="timer">Too lale...</div>;
+      return <div className="timer">next</div>;
     }
-  
+
     return (
       <div className="timer">
-        <div className="text">Remaining</div>
         <div className="value">{remainingTime}</div>
-        <div className="text">seconds</div>
       </div>
     );
   };
- 
 
   return (
-    
     isAuthenticated && (
-      <div>
-
-        <div className="exercise-container">
-          <img
+      <div className="workout-container">
+        <div className="timer-container">
+        <CountdownCircleTimer
+              key={key}
+              size={120}
+              isPlaying={startedTimer}
+              duration={timer}
+              colors={["#DDBAD1", "#FFB2B6","#F57A9B","#D1355F"]}
+              colorsTime={[30, 15, 7, 0]}
+              onComplete={() => {
+                setNumber(number + 1);
+                audio.play();
+                setBtnStatusStart(false)
+              }}
+            >
+              {renderTime}
+            </CountdownCircleTimer>
+         
+        </div>
+      
+        <div className="container-info">
+        <img
             src={exercise[number].picture}
             alt="exercisePic"
             className="exercisePic"
           />
-        </div>
+            </div>
 
-        <div className="container-info">
-         
           <h2 className="exercise-name">{exercise[number].nameOfExercise}</h2>
-          <div className="time-counter">
-           
-        <CountdownCircleTimer
-         key={key}
-    isPlaying={startedTimer}
-    duration={timer}
-    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-    colorsTime={[7, 5, 2, 0]}
-    onComplete={() => {
-      setNumber(number + 1);
-      audio.play()
-    
-    } }
-  >
-     {renderTime}
-  </CountdownCircleTimer>
-
-   <button onClick={() => setKey((prevKey) => prevKey + 1)}>
-          Restart Timer
-        </button>
-          </div>
-        </div>
+        
+      
         <div className="button-container">
-          <div onClick={startTimer} className="playBtn-container">
-            <img
-              src={playBtnPic}
-              alt="btnStart"
-              className="playBtnPic"
-              disabled={btnStatusStart}
-            />
-          </div>
-
-          <div onClick={stopTimer} className="stopBtn-container">
-            <img
-              src={stopBtnPic}
-              alt="btnStop"
-              className="stopBtnPic"
-              disabled={btnStatusStop}
-            />
-          </div> 
-        </div>
-        <div>
        
-      </div>
-   
+          <button onClick={stopTimer} className="stopBtn-container" >
+          {srcPic}
+          </button>
+          <button onClick={startTimer} className="stopBtn-container" disabled={btnStatusStart}>
+            Start
+          </button>
+     
+        </div>
+        <div></div>
       </div>
     )
   );
