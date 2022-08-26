b3a2bf4b776a8a196d724f0941ebbabe8639d892
Lilly <lilly@DESKTOP-I6QR8AT.(none)>
2022-08-26 14:17:05 +0200

    restart possible

--- src/pages/StretchWorkout.js
+++ src/pages/StretchWorkout.js
@@ -25,10 +25,7 @@ const StretchWorkout = (props) => {
   const [startedTimer, setStartedTimer] = React.useState(false);
 
   const { isAuthenticated } = useAuth0();
-
-  const { token } = props;
-
-
+  const [key, setKey] = useState(0);
 
   //Use RefHook to make it stop at 0
   //stores reference to SetInterval --> clear when timer hits 0
@@ -40,8 +37,9 @@ const StretchWorkout = (props) => {
 
   //Start Timer on Button Click --> startBtn
   const startTimer = () => {
-   
+
     setStartedTimer(true)
+
    /*  id.current = setInterval(() => {
       setTimer((time) => time - 1);
     
@@ -79,9 +77,23 @@ const StretchWorkout = (props) => {
     setBtnStatusStop(true); */
   };
 
-
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="timer">Too lale...</div>;
+    }
+  
+    return (
+      <div className="timer">
+        <div className="text">Remaining</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">seconds</div>
+      </div>
+    );
+  };
+ 
 
   return (
+    
     isAuthenticated && (
       <div>
 
@@ -99,18 +111,23 @@ const StretchWorkout = (props) => {
           <div className="time-counter">
            
         <CountdownCircleTimer
+         key={key}
     isPlaying={startedTimer}
-    duration={exercise[number].duration}
+    duration={timer}
     colors={['#004777', '#F7B801', '#A30000', '#A30000']}
     colorsTime={[7, 5, 2, 0]}
     onComplete={() => {
       setNumber(number + 1);
       audio.play()
-      return { shouldRepeat: true, delay: 1.5 } // repeat animation in 1.5 seconds
+    
     } }
   >
-    {({ remainingTime }) => remainingTime}
+     {renderTime}
   </CountdownCircleTimer>
+
+   <button onClick={() => setKey((prevKey) => prevKey + 1)}>
+          Restart Timer
+        </button>
           </div>
         </div>
         <div className="button-container">
