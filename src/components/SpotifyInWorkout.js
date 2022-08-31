61d3e7476b023224a6db8db91f2f4f419d368c87
Lilly <lilly@DESKTOP-I6QR8AT.(none)>
2022-08-31 14:30:35 +0200

    show chosen playlist

--- src/components/SpotifyInWorkout.js
+++ src/components/SpotifyInWorkout.js
@@ -4,13 +4,14 @@ import SpotifyPlayer from "react-spotify-web-playback";
 const SpotifyInWorkout = (props) => {
 
     const {token} = props;
+    const {playlistId} = props;
     console.log(props)
   
   return (
     <div >   <SpotifyPlayer
     className='spotifyPlayer'
     token={token}
-    uris={[`spotify:playlist:1hbvNy48i7cJ5rVn2KOkzl`]}
+    uris={[playlistId]}
   />;
     </div>
   )
